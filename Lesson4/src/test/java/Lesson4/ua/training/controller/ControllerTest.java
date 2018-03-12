32eb881f683adc9b7fcf8e886816727e5c2ead37
Lanfirus <Andrey.dudchenko@gmail.com>
2018-03-12 13:49:18 +0200

    Added more JUnit tests

--- Lesson4/src/test/java/Lesson4/ua/training/controller/ControllerTest.java
+++ Lesson4/src/test/java/Lesson4/ua/training/controller/ControllerTest.java
@@ -53,6 +53,10 @@ public class ControllerTest extends Assert{
         Assert.assertEquals(expected, actual);
     }
 
+    /**
+     * Here and later all tests for Surname are the same as for Name and Patronymic.
+     * The only difference is that Patronymic is not obligatory, so it could be absent
+     */
     @Test
     public void matchInputWithRegexpUkrSurnameCorrectSimple() {
         String test = "Петренко";
@@ -146,6 +150,179 @@ public class ControllerTest extends Assert{
 
         boolean actual = controller.matchInputWithRegexp(test, regexp);
         boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpUkrSurnameIcnorrectEndsWithCapitalLetter() {
+        String test = "ПетренкоП";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(0));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpUkrSurnameIcnorrectSeveralSpecialSymbolsConsecutive() {
+        String test = "Петренко''";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(0));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpUkrSurnameIcnorrectSeveralCapitalLettersConsecutive() {
+        String test = "ПетренкоПВава";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(0));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpUkrPatronymicCorrectNull() {
+        String test = "";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(2));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
+        Assert.assertTrue(expected == actual);
+    }
+
+
+    @Test
+    public void matchInputWithRegexpNicknameCommentSkypeCorrect() {
+        String test = "g";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(4));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpNicknameCommentSkypeIncorrectNull() {
+        String test = "";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(4));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    /**
+     * Here and later all tests are the same for home, mobile and additional mobile numbers.
+     * Additional mobile number is optional
+     */
+    @Test
+    public void matchInputWithRegexpPhoneCorrectUkrainianFormat() {
+        String test = "380441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneCorrect2DigitCountryCode() {
+        String test = "80441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneCorrect1DigitCountryCode() {
+        String test = "8441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneIncorrectLessThan10Digits() {
+        String test = "441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneIncorrectMoreThan12Digits() {
+        String test = "3800441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneIncorrectNull() {
+        String test = "3800441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpPhoneIncorrectNotDigitSymbols() {
+        String test = "38o441234567";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(7));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = false;
+        Assert.assertTrue(expected == actual);
+    }
+
+    @Test
+    public void matchInputWithRegexpAdditionalMobilePhoneCorrectNull() {
+        String test = "";
+        controller.bundleInitialization(new Locale("ua"));
+        List<String> regexpArray = Regexp.returnRegexpStream().collect(Collectors.toList());
+        String regexp = controller.getRegexpBundle().getString(regexpArray.get(9));
+
+        boolean actual = controller.matchInputWithRegexp(test, regexp);
+        boolean expected = true;
         Assert.assertTrue(expected == actual);
     }
 
